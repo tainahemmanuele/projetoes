20a4fee72dad4495a894507ff2450ac0837e4046
Caio Marinho <Caio Marinho>
2017-07-22 18:10:44 -0300

    Comecando os trabalhos

--- app/src/main/java/com/sugar/collection/collectionsugar/activities/MainActivity.java
+++ app/src/main/java/com/sugar/collection/collectionsugar/activities/MainActivity.java
@@ -1,13 +1,17 @@
 package com.sugar.collection.collectionsugar.activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.sugar.collection.collectionsugar.R;
 import com.sugar.collection.collectionsugar.R.id;
@@ -29,10 +33,16 @@ public class MainActivity extends AppCompatActivity {
         Utils.generateCategoryDefault();
         List<Collection> listCollectionUser = CollectionService.getCollectionsByUser
                 (SettingsService.USER_TEMPORARY_SESSION.getId().intValue());
-        ListView lisViewCollection = (ListView) this.findViewById(id.lv_collection);
+        ListView listViewCollection = (ListView) this.findViewById(id.lv_collection);
         ListCollectionAdapter customAdapter = new ListCollectionAdapter(this, layout
                 .item_list_row, listCollectionUser);
-        lisViewCollection.setAdapter(customAdapter);
+        listViewCollection.setAdapter(customAdapter);
+
+        listViewCollection.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+               // TODO: Navigate to view Item With delete button.
+            }
+        });
 
         FloatingActionButton btnLogout = (FloatingActionButton) this.findViewById(id.fab_logout);
         FloatingActionButton btnAddCollection = (FloatingActionButton) this.findViewById(id.fab);
@@ -69,6 +79,30 @@ public class MainActivity extends AppCompatActivity {
     public void logout() {
         SessionService.deleteAllSessions();
         SettingsService.USER_TEMPORARY_SESSION = null;
+    }
+
+    // TODO: Method vai para View Item, pois botao de deletar estará lá.
+    public void showAlertDelete(final int idCollection){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Deseja realmente deletar este item ?")
+                .setCancelable(false)
+                .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        if(CollectionService.deleteCollection(idCollection)){
+                            Toast toast = Toast.makeText(getApplicationContext(), R.string
+                                            .item_delete_success,
+                                    Toast.LENGTH_SHORT);
+                            toast.show();
+                        }else{
+                            Toast toast = Toast.makeText(getApplicationContext(), R.string
+                                            .item_delete_error,
+                                    Toast.LENGTH_SHORT);
+                            toast.show();
+                        }
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 
 
